41e6d18fc31df30a5f561215e9aba29656363afb
Matheus Martins <matheusmmartins8@gmail.com>
2022-05-24 16:55:30 -0300

    Organização de diretórios e arquivos acrescentados

---
+++ preparando_ambiente_node_ubuntu/script.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Executar comandos a seguir para atualizar os pacotes
+sudo apt update -y
+sudo apt upgrade -y
+
+# Instalar pacotes a seguir
+sudo apt install dkms make perl gcc build-essential git curl -y
+
+# Remove qualquer instalação anterior do Node.js (se existir)
+sudo apt purge --auto-remove nodejs -y
+sudo rm /etc/apt/sources.list.d/nodesource.list*
+
+# Baixar e instalar VS Code: https://code.visualstudio.com/download
+
+# Baixa e instala o Node.js na versão 16
+curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
+sudo apt install nodejs -y
+
+# Abaixo tudo é opcional
+
+# Instalar e configurar ZSH
+sudo apt install zsh -y
+chsh -s /bin/zsh
+zsh
+
+# Instalar Oh-my-zsh! -> https://ohmyz.sh/
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Instalar Spaceship Prompt
+# https://github.com/spaceship-prompt/spaceship-prompt
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+# Mudar ~/.zshrc -> ZSH_THEME="spaceship"
+
+# Instalar Zsh Autosuggestions
+# https://github.com/zsh-users/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Instalar Zsh Syntax Highlighting
+# https://github.com/zsh-users/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Mudar plugins
+# plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
+
+# Font optional (https://github.com/pdf/ubuntu-mono-powerline-ttf)
+mkdir -p ~/.fonts
+git clone https://github.com/pdf/ubuntu-mono-powerline-ttf.git ~/.fonts/ubuntu-mono-powerline-ttf
+fc-cache -vf
+
+reboot
